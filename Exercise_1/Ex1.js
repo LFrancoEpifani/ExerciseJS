5928cd80fc62235bef1eabe291f4ac3acf1279a1
LucianoEsplai <lfrancoepifani@gmail.com>
2023-10-14 17:04:50 +0200

    Ex5

---
+++ Exercise_1/Ex1.js
@@ -0,0 +1,27 @@
+// Exercise Nº1
+
+// Write a one-line program that show an alert to appear on the screen that says "Hello World"
+
+console.log(alert('Hello world'));
+
+// Exercise Nº2
+
+// Write a one-line program that write on screen "Hello world" (document.write)
+
+console.log(document.write('Hello world'));
+
+// Exercise Nº3
+
+// Write a one-line program that write on screen 3 + 5;
+
+console.log(document.write("<br>", 3 + 5));
+
+// Exercise Nº4
+
+// Write a program in two lines that ask to user for name with a prompt and write a text that says "Hello userName";
+
+let person = prompt("What's your name?");
+
+document.write('<br>Hello\n' + person);
+
+// Exercise N5°
